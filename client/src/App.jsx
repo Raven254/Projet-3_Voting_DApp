ae0a17fae5cef51f9ab875b92119cf6a3f72718d
0xMeurwo <marwaane.eljaafari@gmail.com>
2022-07-19 00:48:21 +0200

    Modif des components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,28 @@
 import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
+import {useState} from "react";
+import React from "react";
+import Header from "./components/Header/";
 import Footer from "./components/Footer";
+import Admin from "./components/Admin";
+import Proposals from "./components/Proposals";
+import Status from "./components/Status";
+import Voter from "./components/Voter";
+import Voting from "./contracts/Voting.json";
+
+
 import "./App.css";
 
 function App() {
   return (
     <EthProvider>
       <div id="App" >
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
+        <Header /> {/*contient <status>*/} <Status />
+        <hr />
+        <Admin /> <Voter /> {/* Afficher Admin si owner, sinon voter*/}
+        <hr />
+        <Proposals />
+        <hr />
+        <Footer />
       </div>
     </EthProvider>
   );
