ecc29a7a2a261e7778cbf6a927e9c90fbea2cd02
0xMeurwo <marwaane.eljaafari@gmail.com>
2022-07-19 08:11:18 +0200

    Ajout du composant Proposals

--- client/src/components/Proposals/Proposals.jsx
+++ client/src/components/Proposals/Proposals.jsx
@@ -1,2 +1,25 @@
 import React from "react";
 import "./Proposals.css";
+
+function allProposals( { proposals }) {
+    return (
+      <div>
+        <h3>Liste des propositions</h3>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Description</th>
+            </tr>
+            {proposals.map((proposal) => (
+                <tr>
+                    <td>{proposals.returnValues._proposalId}</td>
+                    <td>{proposals.returnValues._desc}</td>
+                </tr>
+            )
+              )});
+        </table>
+      </div>
+    );
+}
+
+export default Proposals;
