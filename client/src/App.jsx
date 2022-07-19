5c675042609d01d620bbb34089e8460482df6e94
0xMeurwo <marwaane.eljaafari@gmail.com>
2022-07-19 05:13:20 +0200

    Ajouts au niveau des components mais bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,57 @@
-import { EthProvider } from "./contexts/EthContext";
-//import {useState} from "react";
-import React from "react";
-//import Header from "./components/Header/";
+import EthProvider from "./contexts/EthContext/EthProvider";
+import useEth from "./contexts/EthContext/useEth";
+import getWeb3 from "./getWeb3";
+import React, { Component, useState, useEffect } from "react";
+import Header from "./components/Header/Header";
 //import Footer from "./components/Footer";
 //import Admin from "./components/Admin";
 //import Proposals from "./components/Proposals";
 //import Status from "./components/Status";
 //import Voter from "./components/Voter";
-//import Voting from "./contracts/Voting.json";
-
+import Voting from "./contracts/Voting.json";
 
 import "./App.css";
 
 function App() {
+  const { state: { contract, accounts } } = useEth();
+  //const [WorkflowStatus, setWorkflowStatus] = useState(0);
+  const [Owner, setOwner] = useState(0);
+  const [Authorized, setAuthorized] = useState(false);
+
+  //console.log("Le owner est : " + Owner);
+
+
+  useEffect ( () => {
+    const Init = async () => { 
+      const owner = await contract.methods.owner().call();
+      console.log("Le owner est : " + owner);
+      setOwner(owner);
+
+   //   const workflowStatus = await contract.methods.getWorkflowstatus().call();
+   //   console.log("Le WorkflowStatus est : " + workflowStatus);
+   //   setWorkflowStatus(workflowStatus);
+    
+      const isVoter = async () => {
+        const voter = await contract.methods.getVoter(accounts[0]).call();
+        setAuthorized(voter[0]);
+      };
+
+    }}, []);
+ 
+  //const isVoter = async() => {
+  //  const {accounts, contract} = this.state;
+  //  const voter = await contract.methods.getVoter(accounts[0]).call();
+  //  setAuthorized(voter[0]);
+ // }
+
+  //if (!web3) {
+  //  return <div>Loading Web3, accounts, and contract...</div>;
+ // }
   return (
     <EthProvider>
       <div id="App" >
-        <h1>Bonjour</h1>
+        <h1>Bienvenue</h1>
+        <Header adresse={accounts} owner={Owner} voter={Authorized} />
         {/*<Header /> {/*contient <status> <Status />
         <hr />
         <Admin /> <Voter /> {/* Afficher Admin si owner, sinon voter
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,25 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { Component, useEffect } from "react";
+import useEth from "../../contexts/EthContext/useEth";
+import Voting from "../../contracts/Voting";
 import "./Header.css";
 
-const Adresse = () => {
-    const [Address, setAddress] = useState();
 
-    useEffect( () => {
-
-    }, []);
-
+const Header = (props) => {
+    const {state: { accounts }} = useEth();
+    const owner = (props.adresse === props.owner);
+    const voter = (props.adresse === props.voter);
     return (
-        <div className="Address">
-
-        </div>
+        <><>
+            <div className="Titre">
+                <h1>Voting DApp</h1>
+                <h2>Edition { owner ? "Owner" : voter ? "Whitelisté" : "Publique"}</h2>
+            </div>
+        </><>
+            <div className="Adresse">
+                <p> Votre adresse: {accounts}</p> 
+            </div>
+        </></>
     )
 }
+
+export default Header;
